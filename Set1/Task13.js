b909250099179291f7c71b5a92943b0ac8e3dbd6
Divina-John <divina.j@btech.christuniversity.in>
2026-07-23 10:45:31 +0530

    Task 13: Student Record System — Format Output with Template Literals

---
+++ Set1/Task13.js
@@ -0,0 +1,6 @@
+let studentName = "John";
+let age = 20;
+let marks = 90;
+console.log(`Student Name: ${studentName}, Age: ${age}, Marks: ${marks}`);
+// Output:
+// Student Name: John, Age: 20, Marks: 90
