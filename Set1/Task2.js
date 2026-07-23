a8d8ca17e9d4ed687e52038193e18794d4a32f43
Divina-John <divina.j@btech.christuniversity.in>
2026-07-23 10:38:46 +0530

    Task 2: Student Record System — Declare and Update Variables

---
+++ Set1/Task2.js
@@ -0,0 +1,7 @@
+let systemName = "Student Record System";
+let totalStudents = 50;
+console.log("System Name:", systemName);
+console.log("Total Students:", totalStudents);
+// Output:
+// System Name: Student Record System
+// Total Students: 50
