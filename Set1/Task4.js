d67a24fe6382fd42d1afd8f57e81ef0d1a56d1bb
Divina-John <divina.j@btech.christuniversity.in>
2026-07-23 10:40:40 +0530

    Task 4: Student Record System — Perform a Calculation (2 Marks)

---
+++ Set1/Task4.js
@@ -0,0 +1,7 @@
+let student1Marks = 80;
+let student2Marks = 90;
+let student3Marks = 85;
+let average = (student1Marks + student2Marks + student3Marks) / 3;
+console.log("Average Marks:", average);
+// Expected Output:
+// Average Marks: 85
